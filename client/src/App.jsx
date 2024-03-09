549dc813be2748a23fb9fd74ffa521bce51fb057
Ganaik2004 <137753416+Ganaik2004@users.noreply.github.com>
2024-03-10 01:14:47 +0530

    Revert "new stuff"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,56 +1,30 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import SideBar from './components/SideBar';
-import HomePage from './components/HomePage';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Profile from './pages/Profile';
-import CreateListing from './pages/CreateListing';
-import PrivateRoute from './components/PrivateRoute';
-import UpdateListings from './pages/UpdateListings';
-import MapComponent from './pages/Events';
-import Categories from './pages/Category';
-import Cricket from './pages/category/Cricket';
-import Badminton from './pages/category/Badminton';
-import Football from './pages/category/Football';
-import Basketball from './pages/category/Basketball';
-import Tennis from './pages/category/Tennis';
-import Accessories from './pages/Accessories';
-import Connect from './pages/connect';
-
+import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import CreateListing from './pages/CreateListing'
+import PrivateRoute from './components/PrivateRoute'
+import UpdateListings from './pages/UpdateListings'
 function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <div className="flex">
-        <SideBar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/sign-in" element={<SignIn />} />
-            <Route path="/sign-up" element={<SignUp />} />
-            <Route path="/events" element={<MapComponent />} />
-            <Route path="/categories" element={<Categories />} />
-            <Route path="/category/cricket" element={<Cricket />} />
-            <Route path="/category/badminton" element={<Badminton />} />
-            <Route path="/category/football" element={<Football />} />
-            <Route path="/category/basketball" element={<Basketball />} />
-            <Route path="/category/tennis" element={<Tennis />} />
-            <Route path="/accessories" element={<Accessories />} />
-            <Route path="/connect" element={<Connect />} />
-            <Route element={<PrivateRoute />}>
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/create-listing" element={<CreateListing />} />
-              <Route path="/update-listing/:listingID" element={<UpdateListings />} />
-            </Route>
-          </Routes>
-        </main>
-      </div>
-      <Footer />
-    </BrowserRouter>
-  );
+  return <BrowserRouter>
+  <Header/>
+  <Routes>
+   <Route path="/" element={<Home/>}/>
+   <Route path="/sign-in" element={<SignIn/>}/>
+   <Route path="/sign-up" element={<SignUp/>}/>
+   <Route path="/about" element={<About/>}/>
+   <Route element={<PrivateRoute/>}>
+   <Route path="/profile" element={<Profile/>}/>
+   <Route path="/create-listing" element={<CreateListing/>}/>
+   <Route path="/update-listing/:listingID" element={<UpdateListings/>}/>
+   </Route>
+  </Routes>
+  <Footer/>
+  </BrowserRouter>
 }
-
-export default App;
+export default App
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,19 +1,7 @@
-import React from 'react';
+import React from 'react'
 
-
-function Footer() {
-    return (
-        <section className="bg-slate-900 text-white text-center">
-            <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-4 text-center">
-        
-            <div className="mt-8 text-center h-6 m-auto">
-                <p className="text-gray-400  text-sm">
-                    © 2024 SPORTS | useDevs
-                </p>
-            </div>
-            </div>
-        </section>
-    );
+export default function Footer() {
+  return (
+    <div>Footer</div>
+  )
 }
-
-export default Footer;
